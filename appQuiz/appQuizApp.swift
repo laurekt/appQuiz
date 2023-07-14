8b967551f42c801094d5fe035d5aa38ba8f2037c
laurekt <laurenvien2006@gmail.com>
2023-07-14 10:35:23 -0700

    Initial Commit

---
+++ appQuiz/appQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  appQuizApp.swift
+//  appQuiz
+//
+//  Created by Scholar on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct appQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
